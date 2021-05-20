ecb9aec2fb4559168afef890ec1d20c0e2aa4997
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-05-20 19:12:38 +0545

    First commit of this repo

---
+++ src/components/finddet.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import {Showdet} from './showdet';
+
+export const Finddet = () => {
+    const [findInput, setFindInput] = useState('');
+    const [repos, setRepos] = useState([]);
+
+    const goChange=(e)=>{
+        setFindInput(e.target.value);
+    }
+    const goClick=async()=>{
+        console.log(findInput);
+        try{
+        const details =await axios(`https://api.github.com/users/${findInput}/repos`);
+        setRepos(details);
+        }
+        catch(e){
+            console.log("error occurred");
+        }
+    };
+    return (
+        <>
+        <div>
+            <input type="text" placeholder="Type the Username Here!" value={findInput} onChange={goChange} />
+            <br />
+            <button onClick={goClick}>Search</button>
+        </div>
+        <Showdet repos={repos} naming={findInput}/>
+        </>
+    )
+}
---
+++ src/components/showdet.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Showdet = (props) => {
+    const {repos} = props;
+    //console.log(props.naming);
+    // console.log(repos.data.length);
+    // const repolist = <h1>Hi</h1>;
+    // for(let i=0;i<repos.data.length;i++){
+    //     console.log(repos.data[i].name);
+    // }
+    if(props.naming===""){
+        return <>Enter Some Value</>
+    }
+    else{
+     repolist = repos.data.length===0? <li>Not Matching</li> : repos.data.map((item)=>{
+        return <li>{item.name}</li>
+    })
+    
+    return (
+        <>
+        Public Repositories of {props.naming}:
+           <ul>
+               {repolist}
+           </ul>
+        </>  
+    )
+    }    
+}
+

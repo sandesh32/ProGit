fdafd75aab666e1f8435657dc0b550f47e03f841
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-05-20 19:33:32 +0545

    Working

--- src/components/showdet.js
+++ src/components/showdet.js
@@ -3,28 +3,28 @@ import React from 'react';
 export const Showdet = (props) => {
     const {repos} = props;
     // console.log(props.naming);
-     console.log(repos);
+     console.log(repos.data);
     // const repolist = <h1>Hi</h1>;
     // for(let i=0;i<repos.data.length;i++){
     //     console.log(repos.data[i].name);
     // }
-    // if(props.naming===""){
-    //     return <>Enter Some Value</>
-    // }
-    // else{
-    // const repolist = repos.data.length===0? <li>Not Matching</li> : repos.data.map((item)=>{
-    //     return <li>{item.name}</li>
-    // })
+     if(props.naming===""){
+         return <>Enter Some Value</>
+     }
+     else{
+     const repolist = repos.data.length===0? <li>Not Matching</li> : repos.data.map((item)=>{
+         return <li>{item.name}</li>
+     })
     
-    // return (
-    //     <>
-    //     Public Repositories of {props.naming}:
-    //        <ul>
-    //            {repolist}
-    //        </ul>
-    //     </>  
-    // )
-    // }    
-    return <h1>hi</h1>
+     return (
+         <>
+         Public Repositories of {props.naming}:
+            <ul>
+                {repolist}
+            </ul>
+         </>  
+     )
+     }    
+    
 }
 
